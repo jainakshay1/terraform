5c258a1658fa67eca7880a674fc9772d0c146f32
jainakshay1 <37770903+jainakshay1@users.noreply.github.com>
2023-01-27 15:47:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,3 @@
-new content of the file
+const newContent = oldContent.replace("old_resource_name", new_resource_name)
+                             .replace("old_region", new_region);
+const content = Buffer.from(newContent).toString("base64");
